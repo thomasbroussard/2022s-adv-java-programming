0f94983e234001b2f825e5bc4f032180644bb1c7
Thomas <thomas.broussard@gmail.com>
2022-12-05 10:35:30 +0100

    * added path param + response entity

--- quiz-rest/src/main/java/fr/epita/quiz/QuizController.java
+++ quiz-rest/src/main/java/fr/epita/quiz/QuizController.java
@@ -1,13 +1,33 @@
 package fr.epita.quiz;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import fr.epita.datamodel.Question;
+import fr.epita.services.QuestionDAO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.inject.Inject;
+import javax.websocket.server.PathParam;
 
 @RestController
 public class QuizController {
 
-    @GetMapping(path="/test")
-    public void test(){
-        System.out.println("hello!");
+//    @Inject
+//    QuestionDAO dao;
+
+    @GetMapping(path="/questions/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Question> get(@PathParam("id") String id){
+        System.out.println("hello from get!");
+        return ResponseEntity.ok(new Question("test"));
+    }
+
+
+    @GetMapping(path="/questions")
+    public void get(){
+        System.out.println("hello from get!");
+    }
+    @PostMapping(path="/test")
+    public void post(){
+        System.out.println("hello from post!");
     }
 }
